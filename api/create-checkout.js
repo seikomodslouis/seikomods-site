13ac4e16ce735778ff1c8a6cc76c9866fba134c2
seikomodslouis <seikomods.louis@gmail.com>
2026-06-03 22:04:59 +0200

    Update create-checkout.js

--- api/create-checkout.js
+++ api/create-checkout.js
@@ -67,6 +67,7 @@ module.exports = async (req, res) => {
       mode: 'payment',
       success_url: `${req.headers.origin || 'https://seikomods-louis.fr'}/merci.html?session_id={CHECKOUT_SESSION_ID}`,
       cancel_url: `${req.headers.origin || 'https://seikomods-louis.fr'}/panier.html`,
+      allow_promotion_codes: true,
       locale: 'fr',
       shipping_address_collection: {
         allowed_countries: ['FR', 'BE', 'CH', 'LU', 'MC'],
